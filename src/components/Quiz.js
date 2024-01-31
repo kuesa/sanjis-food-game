35d459f666116dc56ade5feaaf4f56d2ad4eb98c
Noah <32714062+kuesa@users.noreply.github.com>
2024-01-30 16:03:21 -0800

    end state

--- src/components/Quiz.js
+++ src/components/Quiz.js
@@ -37,6 +37,29 @@ export function Quiz() {
             <span className="font-medium">{questionAnswered === 2 ? "CORRECT ANSWER! C'est Magnifique!!!" : "WRONG ANSWER! Sacre Bleu!!"}</span>
         </div>
         : null;
+
+    let color;
+    let reaction;
+
+    if (score === 0) {
+        color = 'red-600';
+        reaction = 'OH MON DIEU!! YOU STINK!';
+    } else if (score < 3) {
+        color = 'amber-700';
+        reaction = 'SACRE BLEU! WHAT A HORRIBEAULX SCORE!';
+    } else if (score < 7) {
+        color = 'yellow-500';
+        reaction = 'NOT VERY GEAUXD. BACK TO ZE KITCHEN!!!';
+    } else if (score < 10) {
+        color = 'yellow-400';
+        reaction = 'Ahhhh it was eaux-kay, but not good enough!';
+    } else if (score === 10) {
+        color = 'green-500';
+        reaction = "MAGNIFIQUE!!! MERVEILLEUX!! C'EST PARFAIT!!";
+    }
+
+    let scoreClass = `mb-2 text-4xl font-bold tracking-tight text-${color}-800 dark:text-${color} text-center`;
+
     return (
         <>
             {!finishedQuiz ? (
@@ -51,7 +74,15 @@ export function Quiz() {
                     </div>
                 </div>
             ) : (
-                <h1>h</h1>
+                <div className="max-w-sm p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
+                    <div className="flex flex-col items-center">
+                        <h1 className="mb-2 text-2xl font-bold tracking-tight text-gray-900 dark:text-white text-center">C'est Fini!</h1>
+                        <img className="object-cover w-full h-48 md:w-48" src="https://c.tenor.com/d-3I9WL9NKYAAAAC/tenor.gif" />
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">Congrateauxlations! You have finished? But how did you do? You got:</p>
+                        <h1 className={scoreClass}>{score} / {Questions.length}</h1>
+                        <p className="mb-3 font-normal text-gray-700 dark:text-gray-400 text-center">{reaction}</p>
+                    </div>
+                </div>
             )
             }
         </>
